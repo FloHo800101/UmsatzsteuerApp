84d9eb11483a27b7341ef3f395a274c38f5c9bf7
Florian Hoffmann <florianhoffmann@Mac.fritz.box>
2025-11-29 07:49:00 +0100

    test: render Home directly and assert heading & Upload button

--- UmsatzsteuerApp_Monorepo_Starter/frontend-belegpruefung/src/__tests__/App.test.tsx
+++ UmsatzsteuerApp_Monorepo_Starter/frontend-belegpruefung/src/__tests__/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import App from '../App'
+import Home from '../pages/Home'
 
-test('zeigt BelegprüfungsApp Titel', () => {
-  render(<App />)
-  // prefer to search by role for robustness
+test('zeigt BelegprüfungsApp Titel auf Home', () => {
+  render(<Home />)
   expect(screen.getByRole('heading', { name: /BelegprüfungsApp/i })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /Upload/i })).toBeInTheDocument()
 })
