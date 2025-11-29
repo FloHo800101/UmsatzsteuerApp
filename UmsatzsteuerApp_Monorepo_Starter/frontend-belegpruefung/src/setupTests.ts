90ace9637b80958b9439c1c8cf41d00345a933b7
Florian Hoffmann <florianhoffmann@Mac.fritz.box>
2025-11-29 07:47:42 +0100

    test: add jsdom env & polyfill for matchMedia; verbose vitest

--- UmsatzsteuerApp_Monorepo_Starter/frontend-belegpruefung/src/setupTests.ts
+++ UmsatzsteuerApp_Monorepo_Starter/frontend-belegpruefung/src/setupTests.ts
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom'
+
+// Polyfills for common browser APIs used by some components
+declare global {
+	interface Window {
+		matchMedia?: (query: string) => MediaQueryList
+	}
+}
+
+if (typeof window.matchMedia !== 'function') {
+	window.matchMedia = (query: string) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: () => {},
+		removeListener: () => {},
+		addEventListener: () => {},
+		removeEventListener: () => {},
+		dispatchEvent: () => false,
+	} as unknown as MediaQueryList)
+}
+
